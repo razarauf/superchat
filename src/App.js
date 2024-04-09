8ea437e3bb3aeb3394e13be70c6188932cdda2f8
Raza Rauf <m.razarauf@gmail.com>
2024-04-08 20:15:30 -0600

    Adds ability to sign in using firebase and the ability to read and write data to the firebase database

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./index.css"
 
-function App() {
+import {firebaseConfig} from './config.js'
+
+// firebase sdk
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+//firebase hooks
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp(firebaseConfig);
+
+export default function App() {
+
+  const auth = firebase.auth();
+  const firestore = firebase.firestore();
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {auth.currentUser && <SignOut />}
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn/> }
+      </section>
     </div>
   );
-}
 
-export default App;
+  function ChatRoom(){
+    const messagesRef = firestore.collection("messages");
+    const query = messagesRef.orderBy("createdAt").limit(25);
+    const [messages] = useCollectionData(query, {idField: 'id'})
+
+    return (
+      <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+      <MessageInput />
+      </>
+    )
+  }
+
+  function MessageInput (prop) {
+    const messagesRef = firestore.collection("messages");
+    const [formValue, setFormValue] = React.useState('')
+    
+    const sendMessage = async (e) => {
+      e.preventDefault();
+
+      const {uid, photoURL} = auth.currentUser;
+
+      await messagesRef.add({
+        text: formValue,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid,
+        photoURL
+      })
+      setFormValue('')
+    }
+
+    return (
+      <>
+      <form onSubmit={sendMessage}>
+        <input onChange={e => setFormValue(e.target.value)} value={formValue} placeholder='say something'/>
+        <button type="submit">Send</button>
+      </form>
+      </>
+    )
+  }
+  
+  function ChatMessage(props){
+    return(<p>{props.message.text}</p>)
+  }
+  
+  function SignIn(){
+    function signInWithGoogle(){
+      const provider = new firebase.auth.GoogleAuthProvider();
+      auth.signInWithPopup(provider);
+    }
+  
+    return (
+      <button onClick={signInWithGoogle}>Sign In</button>
+    )
+  }
+  
+  function SignOut(){
+    return (<button onClick={() => {auth.signOut()}}>Sign Out</button>)
+  }
+  
+}
