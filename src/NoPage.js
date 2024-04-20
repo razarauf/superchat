2d2dbdcf287f92864de0dd1cfdda2d3e7b05e86b
Raza Rauf <m.razarauf@gmail.com>
2024-04-20 04:24:00 -0600

    Refactor

--- src/NoPage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function NoPage () {
-    return (<h3>Page does not exist!</h3>)
-}
--- src/Posts.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Posts () {
-    return (<p>Posts</p>)
-}
